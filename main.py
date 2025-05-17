55b90588ebb1d89314e59246310e708b3e79a0f0
Saurabh Kumar <Saurabhtbj143@gmail.com>
2025-05-17 22:25:55 +0530

    Initial commit

--- main.py
+++ main.py
@@ -160,6 +160,4 @@ def predict():
 
 # Run the app
 if __name__ == '__main__':
-    import os
-    port = int(os.environ.get("PORT", 5000))
-    app.run(debug=False, host="0.0.0.0", port=port)
+    app.run(debug=True)
