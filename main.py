e15a7361a831d0d0e2b3914eb8c512b7594add23
Saurabh Kumar <Saurabhtbj143@gmail.com>
2025-05-17 15:26:25 +0530

    Update for Railway deployment and add imblearn to requirements

--- main.py
+++ main.py
@@ -160,4 +160,6 @@ def predict():
 
 # Run the app
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=False, host="0.0.0.0", port=port)
